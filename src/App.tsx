dc7ba2ce2740abed4371ac85fed174966680a5a5
Dovran <orazowd77@gmail.com>
2025-07-05 14:38:48 +0500

    refactor: reorganize imports and improve code formatting in App and Products components

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import { Footer, Header, Loader } from "./components/layout";
 import ReactLenis, { useLenis } from "lenis/react";
-import { Burger } from "./components/shared";
-import { useLoaderStore } from "./store/use-loader";
 import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { Footer, Header, Loader } from "./components/layout";
+import { Burger } from "./components/shared";
 import { useBurgerStore } from "./store/use-burger";
+import { useLoaderStore } from "./store/use-loader";
 
 function App() {
-  const loading = useLoaderStore((state) => state.isLoading);
-  const isOpen = useBurgerStore((state) => state.isOpen);
-  const lenis = useLenis();
+	const loading = useLoaderStore(state => state.isLoading);
+	const isOpen = useBurgerStore(state => state.isOpen);
+	const lenis = useLenis();
 
-  useEffect(() => {
-    if (isOpen || loading) lenis?.stop();
-    else lenis?.start();
-  }, [isOpen, loading]);
+	useEffect(() => {
+		if (isOpen || loading) lenis?.stop();
+		else lenis?.start();
+	}, [isOpen, loading]);
 
-  useEffect(() => {
-    lenis?.scrollTo(0, { duration: 0, lerp: 0 });
-  }, []);
+	useEffect(() => {
+		lenis?.scrollTo(0, { duration: 0, lerp: 0 });
+	}, []);
 
-  return (
-    <ReactLenis
-      root
-      options={{
-        smoothWheel: true,
-        lerp: 0.08,
-      }}
-    >
-      <div>
-        <Loader />
-        <Burger />
-        <Header />
-        <main className="flex-auto overflow-hidden">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </ReactLenis>
-  );
+	return (
+		<ReactLenis
+			root
+			options={{
+				smoothWheel: true,
+				lerp: 0.08
+			}}
+		>
+			<div>
+				<Loader />
+				<Burger />
+				<Header />
+				<main className="flex-auto overflow-hidden">
+					<Outlet />
+				</main>
+				<Footer />
+			</div>
+		</ReactLenis>
+	);
 }
 
 export default App;
